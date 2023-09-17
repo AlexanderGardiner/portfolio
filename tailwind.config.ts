39d978d5c477c6c65e73e7e59f9fd63d34a8b7ec
AlexanderGardiner <misc@alexandergardiner.com>
2023-09-17 15:19:57 -0700

    Inital commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'navigationSelectedPageColour': '#1e6ae6',
+        'navigationUnselectedPageColour': '#000000',
+      },
     },
   },
   plugins: [],
+  
 }
+
 export default config
