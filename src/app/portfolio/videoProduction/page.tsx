865500dbfb2762d6476a740c38ce45b476d60bc0
AlexanderGardiner <misc@alexandergardiner.com>
2023-11-16 21:28:53 -0800

    Video Portfolio small updates

--- src/app/portfolio/videoProduction/page.tsx
+++ src/app/portfolio/videoProduction/page.tsx
@@ -9,14 +9,14 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN Challenge - Commerical</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 30 second commerical created for the STN Challenge Contest. This project was filmed in around 2 hours and edited in around a day during High School. I was the primary cameramen and the ... editor.</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 30 second commerical created for the STN Challenge Contest. This project was filmed in around 2 hours and edited in around a day during High School. I was the primary cameramen and the primary editor. The prompt was to promote an app that you created, and to include a clear catchphrase in the commerical.</p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/CapitleCommerical.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
                 </video>
             </li>
             <li className="mb-10 ml-6">            
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">60 second Horrorfest Contest</h3>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN 60 second Horrorfest Contest</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023</time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest. This project was filmed and edited in around 2 weeks during High School. I was one of the primary cameramen and the primary editor.</p>
                 <video className="w-full" controls>
@@ -70,7 +70,7 @@ export default function Page() {
                 </video>
             </li>
             <li className="mb-10 ml-6">            
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">60 second Horrorfest Contest</h3>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN 60 second Horrorfest Contest</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2022</time>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest.</p>
                 <video className="w-full" controls>
