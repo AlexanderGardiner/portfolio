c2020f95789d25a75fe01ef9dad12f3f945e5850
AlexanderGardiner <misc@alexandergardiner.com>
2023-11-22 16:10:32 -0800

    Video production portfolio updates

--- src/app/portfolio/videoProduction/page.tsx
+++ src/app/portfolio/videoProduction/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN 60 second Horrorfest Contest</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest. This project was filmed and edited in around 2 weeks during High School. I was one of the primary cameramen and the primary editor.</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest <a className="text-highlight hover:text-white" href="https://studenttelevision.com/sixty-second-horrorfest/">https://studenttelevision.com/sixty-second-horrorfest/</a>. This project was filmed and edited in around 2 weeks during High School. I was one of the primary cameramen and the primary editor. We had to include the line of dialogue "Is it over?", a video from a horror film in the public domain, as well as a mirror.</p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/Horrorfest2023.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
@@ -45,7 +45,7 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">DPNews Intro</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest.</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A short intro created for the high school news program at my school. This intro played on the daily news show during 2023. It showcases life at my high school as well as the news program itself. <a className="text-highlight hover:text-white" href="https://dpnews.org/">https://dpnews.org/</a></p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/DPNewsIntro2023.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
@@ -54,7 +54,7 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN Convention 30 second PSA</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A PSA (Under the High School category) created during the 2023 STN Convention <a className="text-highlight hover:text-white" href="https://www.stnconvention.com/general-contest-winners/">https://stnconvention.com/</a>. Written, filmed and edited in less than 12 hours. I was the primary cameraman and the primary editor. More than 3000 students attended the convention for their high schools. One submission was permitted to this category per school and this video won an honorary mention (4th Place).</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A PSA (Under the High School category) created during the 2023 STN Convention <a className="text-highlight hover:text-white" href="https://www.stnconvention.com/general-contest-winners/">https://stnconvention.com/</a>. Written, filmed and edited in less than 12 hours. I was the primary cameraman, script writer, and editor. More than 3000 students attended the convention for their high schools. One submission was permitted to this category per school and this video won an honorary mention (4th Place).</p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/STNConventionPSA.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
@@ -63,7 +63,7 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Waterwise Contest</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest.</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 30 second PSA created for the Santa Barbara Waterwise video contest. WaterwiseSB is a regional partnership that aims to promote water use efficiency <a className="text-highlight hover:text-white" href="https://waterwisesb.com/">https://waterwisesb.com/</a>. I entered this yearly video contest and was the actor, as well as one of the editors for the project.</p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/Waterwise2023.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
@@ -72,7 +72,7 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN 60 second Horrorfest Contest</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2022</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest.</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest <a className="text-highlight hover:text-white" href="https://studenttelevision.com/sixty-second-horrorfest/">https://studenttelevision.com/sixty-second-horrorfest/</a>. This was filmed in around 2 hours. I was one of the primary editors, as well as the primary videographer. We had to include a box (the elevator was our box of choice), a line ("Don't be scared, just come with me"), and negative space.</p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/Horrorfest2022.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
@@ -81,7 +81,7 @@ export default function Page() {
             <li className="mb-10 ml-6">            
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">STN Challenge - Nat Sound</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2022</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A 60 second horror short film created for the STN Horrorfest Contest.</p>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A video package creater for the 2022 STN Challenge under the category of "Nat Sound" <a className="text-highlight hover:text-white" href="https://studenttelevision.com/stn-challenge-prompts">https://studenttelevision.com/stn-challenge-prompts/</a>. The prompt was Rhythm of the Street and we chose to showcase a local musician who often played at a marketplace nearby. I was one of the primary videographers, and one of the primary editors.</p>
                 <video className="w-full" controls>
                     <source src="/portfolio/videoProjects/STNChallengeNatSound.mp4" type="video/mp4"></source>
                     Your browser does not support playing this video.
