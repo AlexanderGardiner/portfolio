7f089d4cb0164e05cb61964043f6f358ab8cde74
AlexanderGardiner <misc@alexandergardiner.com>
2024-06-09 16:19:45 -0700

    Removed git submodules

--- programmingProjects/staticServer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = 8002;
-
-app.use(express.static('static'));
-  
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
